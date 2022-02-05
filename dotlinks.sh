7f8dcd0d5e73269f36c324dff70007f2ec33b5c3
101ta28 <workta28@gmail.com>
2022-02-05 20:25:08 +0900

    update

--- dotlinks.sh
+++ dotlinks.sh
@@ -4,7 +4,5 @@ ln -sf ~/.dvim/.vim ~/.vim
 ln -sf ~/.dvim/.viminfo ~/.viminfo
 ln -sf ~/.dvim/.vimrc ~/.vimrc
 
-./installer.sh ~/.cache/dein
-
 echo "Use Vim and input command"
 echo ":call dein#install()"
