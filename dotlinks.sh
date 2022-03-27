84886a96a5de405e12c5a92d8f83c64a67839886
Tatsuya Imai <34620490+101ta28@users.noreply.github.com>
2022-03-27 23:00:11 +0900

    Update dotlinks.sh

--- dotlinks.sh
+++ dotlinks.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # dotfiles
+mkdir ~/.vim
+mkdir ~/.vim/undo
 ln -sf ~/.dvim/.vim ~/.vim
 ln -sf ~/.dvim/.viminfo ~/.viminfo
 ln -sf ~/.dvim/.vimrc ~/.vimrc
-
-mkdir ~/.vim/undo
+# dein install
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+rm installer.sh
 
 echo "Use Vim and input command"
 echo ":call dein#install()"
